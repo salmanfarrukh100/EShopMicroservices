573d3dcb3e8500ff0eed8226ae3b5842657ac931
Salman Farrukh <salmanfarrukh100@hotmail.com>
2024-07-03 14:12:12 +0500

    Common Library Added

---
+++ src/Services/Catalog.API/Product/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Product.CreateProduct
+{
+    public class CreateProductEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Catalog.API.Product.CreateProduct
+{
+    public record CreateProductCommand(Guid Id,
+     string Name,
+     List<string> Category,
+     string Description,
+     string ImageFile,
+     decimal price):IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
